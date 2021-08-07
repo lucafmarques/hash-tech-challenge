095213e0fbda1aeb12e0d3b3a044ee5e6d50e427
Luca F. Marques <lucafmarqs@gmail.com>
2021-08-07 20:49:35 -0300

    chore: Add config loading from ENV with fallback

--- go.mod
+++ go.mod
@@ -3,18 +3,18 @@ module gitlab.com/lucafmarques/hash-test
 go 1.16
 
 require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
 	github.com/go-openapi/jsonreference v0.19.6 // indirect
 	github.com/go-openapi/spec v0.20.3 // indirect
 	github.com/go-openapi/swag v0.19.15 // indirect
 	github.com/labstack/echo/v4 v4.5.0
 	github.com/labstack/gommon v0.3.0
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/pkg/errors v0.8.1 // indirect
+	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
-	github.com/swaggo/swag v1.7.0 // indirect
-	github.com/urfave/cli v1.20.0 // indirect
+	github.com/swaggo/echo-swagger v1.1.2
+	github.com/swaggo/swag v1.7.0
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
 	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
 	golang.org/x/tools v0.1.5 // indirect
--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"gitlab.com/lucafmarques/hash-test/auth"
 	"gitlab.com/lucafmarques/hash-test/checkout"
 	"gitlab.com/lucafmarques/hash-test/discount"
 	"gitlab.com/lucafmarques/hash-test/repository"
@@ -19,13 +18,26 @@ var (
 	discountServerAddr = "discount:50051"
 )
 
+// @title Hash's Checkout API
+// @version 1.0.0
+// @description API for receiving cart info and generating a checkout order with proper discounts received by calling Discount service.
+// @contact.name Luca F. Marques
+// @contact.email lucafmarqs@gmail.com
+// @license.name MIT
+
 func main() {
-	grpcOpts := []grpc.DialOption{
-		grpc.WithInsecure(), grpc.WithBlock(),
+	config := &Config{}
+	err := config.LoadFromEnv("CONFIG_PATH", "config.yaml")
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
+
+	grpcOpts := []grpc.DialOption{
+		grpc.WithInsecure(), grpc.WithBlock(),
+	}
 
 	conn, err := grpc.DialContext(ctx, discountServerAddr, grpcOpts...)
 	if err != nil {
@@ -37,13 +49,13 @@ func main() {
 	client := discount.NewDiscountClient(conn)
 	repo, err := repository.NewEmbedRepository()
 	if err != nil {
-		log.Fatalf("Failed loading service repository: %s", err)
+		log.Fatalf("failed loading service repository: %s", err)
 	}
 
 	svc := checkout.NewCheckoutService(*server, client, repo)
 	defer svc.Stop()
 
-	svc.ApplyMiddlewares(middleware.Logger(), middleware.Recover(), auth.AuthMiddleware())
+	svc.ApplyMiddlewares(middleware.Logger(), middleware.Recover())
 	svc.RegisterRoutes()
 
 	svc.Start()
