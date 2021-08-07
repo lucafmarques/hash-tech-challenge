a53c2121b70baa8c30bc76cc40048bf36d90d7d0
Luca F. Marques <lucafmarqs@gmail.com>
2021-08-07 17:07:46 -0300

    feat: Add new handler for checkout route + modify older handlers to use new repository interface

--- handler.go
+++ handler.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 	"strconv"
 
@@ -9,11 +8,11 @@ import (
 	"gitlab.com/lucafmarques/hash-test/discount"
 )
 
-var Data []Product
-
-func init() {
-	json.Unmarshal(Products, &Data)
-}
+var (
+	ErrInvalidProductId       = echo.NewHTTPError(http.StatusBadRequest, "Product ID should be a number.")
+	ErrInvalidPayload         = echo.NewHTTPError(http.StatusBadRequest, "Request body doesn't comply with API spec.")
+	ErrFailedFetchingProducts = echo.NewHTTPError(http.StatusInternalServerError, "Couldn't fetch all products.")
+)
 
 type CheckoutResponse struct {
 	TotalAmount             int               `json:"total_amount"`
@@ -23,7 +22,7 @@ type CheckoutResponse struct {
 }
 
 type ProductResponse struct {
-	Id          int  `json:"id"`
+	ID          int  `json:"id"`
 	Quantity    int  `json:"quantity"`
 	UnitAmount  int  `json:"unit_amount"`
 	TotalAmount int  `json:"total_amount"`
@@ -31,16 +30,17 @@ type ProductResponse struct {
 	Gift        bool `json:"is_gift"`
 }
 
-type DiscountResponse struct {
-	Percentage float32 `json:"percentage"`
+type CheckoutRequest struct {
+	Products []ProductRequest `json:"products"`
 }
 
-type Product struct {
-	Id          int    `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Amount      int    `json:"amount"`
-	Gift        bool   `json:"is_gift"`
+type ProductRequest struct {
+	ID       int `json:"id"`
+	Quantity int `json:"quantity"`
+}
+
+type DiscountResponse struct {
+	Percentage float32 `json:"percentage"`
 }
 
 func (svc *Service) HandleHello(c echo.Context) error {
@@ -48,7 +48,12 @@ func (svc *Service) HandleHello(c echo.Context) error {
 }
 
 func (svc *Service) GetAllProducts(c echo.Context) error {
-	return c.JSON(http.StatusOK, Data)
+	products, err := svc.Repository.GetAllProducts()
+	if err != nil {
+		svc.Server.Logger.Warnf("Failed fetching all products from repository: %v", err)
+		return ErrFailedFetchingProducts
+	}
+	return c.JSON(http.StatusOK, products)
 }
 
 func (svc *Service) GetProductDiscount(c echo.Context) error {
@@ -56,8 +61,8 @@ func (svc *Service) GetProductDiscount(c echo.Context) error {
 
 	id, err := strconv.Atoi((c.Param("id")))
 	if err != nil {
-		svc.Server.Logger.Warnf("Failed conversion of Product id to int: %v", err)
-		return echo.NewHTTPError(http.StatusBadRequest, "Product ID should be a number.")
+		svc.Server.Logger.Warnf("Failed conversion of Product ID to int: %v", err)
+		return ErrInvalidProductId
 	}
 
 	resp := DiscountResponse{
@@ -76,4 +81,46 @@ func (svc *Service) GetProductDiscount(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, resp)
+}
+
+func (svc *Service) PostCheckout(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	data := CheckoutRequest{}
+	if err := c.Bind(&data); err != nil {
+		svc.Server.Logger.Warnf("Failed to unmarshal reques data into %T: %v", data, err)
+		return ErrInvalidPayload
+	}
+
+	response := CheckoutResponse{
+		Products: []ProductResponse{},
+	}
+
+	for _, productRequest := range data.Products {
+		productData, err := svc.Repository.GetProduct(productRequest.ID)
+		if err != nil {
+			svc.Server.Logger.Warnf("Failed requesting product from repository: %v", err)
+			continue
+		}
+
+		productResponse := NewProductResponse(productData, productRequest)
+
+		discountReq := &discount.GetDiscountRequest{
+			ProductID: int32(productRequest.ID),
+		}
+
+		discountResp, err := svc.DiscountClient.GetDiscount(ctx, discountReq)
+		if err != nil {
+			svc.Server.Logger.Warnf("Failed requesting discount from external service: %v", err)
+		} else {
+			productResponse.Discount = CalculateDiscount(productData.Amount, discountResp.GetPercentage())
+		}
+
+		response.TotalAmount += productResponse.TotalAmount
+		response.TotalDiscount += productResponse.Discount
+		response.TotalAmountWithDiscount += productResponse.TotalAmount - productResponse.Discount
+		response.Products = append(response.Products, productResponse)
+	}
+
+	return c.JSON(http.StatusOK, response)
 }
