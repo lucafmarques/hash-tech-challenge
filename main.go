45a8fb7c1bb24524105ff5f8cfcc0a26561124f0
Luca F. Marques <lucafmarqs@gmail.com>
2021-08-07 17:06:16 -0300

    chore: Change how service is setup

--- main.go
+++ main.go
@@ -1,32 +1,45 @@
 package main
 
 import (
+	"context"
 	_ "embed"
 	"log"
+	"time"
 
+	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"gitlab.com/lucafmarques/hash-test/auth"
+	"gitlab.com/lucafmarques/hash-test/discount"
+	"gitlab.com/lucafmarques/hash-test/repository"
 	"google.golang.org/grpc"
 )
 
 var (
-	//go:embed products.json
-	Products           []byte
-	discountServerAddr = "localhost:50051"
+	discountServerAddr = "discount:50051"
 )
 
 func main() {
 	grpcOpts := []grpc.DialOption{
-		grpc.WithInsecure(),
+		grpc.WithInsecure(), grpc.WithBlock(),
 	}
 
-	conn, err := grpc.Dial(discountServerAddr, grpcOpts...)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	conn, err := grpc.DialContext(ctx, discountServerAddr, grpcOpts...)
 	if err != nil {
 		log.Fatalf("fail to dial: %v", err)
 	}
 	defer conn.Close()
 
-	svc := NewCheckoutService(conn)
+	server := echo.New()
+	client := discount.NewDiscountClient(conn)
+	repo, err := repository.NewEmbedRepository()
+	if err != nil {
+		log.Fatalf("Failed loading service repository: %s", err)
+	}
+
+	svc := NewCheckoutService(*server, client, repo)
 	defer svc.Stop()
 
 	svc.ApplyMiddlewares(middleware.Logger(), middleware.Recover(), auth.AuthMiddleware())
