d4769a7a4549c5ff1ce694da2ead741d389ef5d9
Luca F. Marques <lucafmarqs@gmail.com>
2021-08-11 19:03:31 -0300

    test: Add test for discount connection creation

---
+++ discount/discount_test.go
@@ -0,0 +1,56 @@
+package discount
+
+import (
+	"context"
+	"log"
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gitlab.com/lucafmarques/hash-test/config"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/test/bufconn"
+)
+
+const (
+	host    = "bufnet"
+	bufSize = 1024 * 1024
+)
+
+var list *bufconn.Listener
+
+type MockDiscountService struct{}
+
+func (d MockDiscountService) GetDiscount(context.Context, *GetDiscountRequest) (*GetDiscountResponse, error) {
+	return &GetDiscountResponse{}, nil
+}
+
+func (d MockDiscountService) mustEmbedUnimplementedDiscountServer() {}
+
+func init() {
+	lis := bufconn.Listen(bufSize)
+	s := grpc.NewServer()
+	RegisterDiscountServer(s, &MockDiscountService{})
+	go func() {
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("Server existed with error: %v", err)
+		}
+	}()
+}
+
+func dialer(context.Context, string) (net.Conn, error) {
+	return list.Dial()
+}
+
+func TestNewDiscountConn(t *testing.T) {
+	config := config.DiscountConfig{
+		Host:    host,
+		Timeout: 10,
+	}
+
+	opts := []grpc.DialOption{grpc.WithInsecure(), grpc.WithContextDialer(dialer)}
+
+	conn, _, err := NewDiscountConn(config, opts)
+	assert.Nil(t, err, "Failed asserting non-error when creating connection")
+	assert.IsType(t, conn, &grpc.ClientConn{}, "Failed asserting conn type")
+}
--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/labstack/gommon v0.3.0
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/stretchr/testify v1.7.0
 	github.com/swaggo/echo-swagger v1.1.2
 	github.com/swaggo/swag v1.7.0
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
