dadc349c3109f9db6b1103531b16f1339aa11bdf
Luca F. Marques <lucafmarqs@gmail.com>
2021-08-12 19:41:38 -0300

    test: Add tests for config package

---
+++ config/config_test.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewConfig(t *testing.T) {
+	config := NewConfig()
+
+	assert.IsType(t, Config{}, config, "Failed asserting config type")
+	assert.Equal(t, DEVELOPMENT, config.Service.Environment, "Failed asserting default Service.Environment in default Config")
+}
+
+func TestLoadFromEnv(t *testing.T) {
+	config := Config{}
+	env := "TEST"
+	path := "config.yaml"
+	os.Setenv(env, path)
+
+	err := config.LoadFromEnv(env, path)
+
+	assert.Nil(t, err, "Failed asserting nil error")
+}
+
+func TestLoadFromEnvNoEnv(t *testing.T) {
+	config := Config{}
+	path := "config.yaml"
+
+	err := config.LoadFromEnv("INEXISTENT_ENV", path)
+
+	assert.Nil(t, err, "Failed asserting nil error")
+}
+
+func TestLoadFromEnvPathError(t *testing.T) {
+	config := Config{}
+	path := "fake_path_that_shouldnt_exist.go"
+
+	err := config.LoadFromEnv("INEXISTENT_ENV", path)
+
+	assert.Error(t, err, "Failed asserting error reading config file")
+}
+
+func TestLoadFromEnvWrongConfig(t *testing.T) {
+	config := Config{}
+	path := "temp_test_config.wrong"
+	ioutil.TempFile("config", path)
+	defer os.Remove(path)
+
+	err := config.LoadFromEnv("INEXISTENT_ENV", path)
+
+	assert.Error(t, err, "Failed asserting error loading config")
+}
